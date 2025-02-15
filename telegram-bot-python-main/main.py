9a5117fae01be82f1af12db99fa951965c9349d7
Dinizando <dinizando23@gmail.com>
2025-02-15 11:26:16 -0300

    Update main.py

--- telegram-bot-python-main/main.py
+++ telegram-bot-python-main/main.py
@@ -5,13 +5,15 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-# Replace 'TELEGRAM_BOT_TOKEN' with the token you received from BotFather
-TOKEN = os.getenv('TELEGRAM_TOKEN')
+# Load token and messages
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+START_MESSAGE = os.getenv("START_MESSAGE")
+
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
-    bot.reply_to(message, "Hello! I'm a simple Telegram bot.")
+    bot.reply_to(message, START_MESSAGE)  # Responde com a mensagem configurada
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
