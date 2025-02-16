f0caa05b85a160c362cab15ac805e72e96689d1e
Dinizando <dinizando23@gmail.com>
2025-02-15 23:11:44 -0300

    Update main.py

--- telegram-bot-python-main/main.py
+++ telegram-bot-python-main/main.py
@@ -24,7 +24,7 @@ users = set()
 # 🔹 Função para enviar a mensagem de urgência a cada 24 horas
 def send_urgent_message():
     while True:
-        time.sleep(86400)  # 24 horas
+        time.sleep(10)  # 24 horas
         for user_id in users:
             bot.send_message(user_id, START_MESSAGE)
 
